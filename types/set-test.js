2ac32abfff9ef8e22e3f1ea32701eb69e9a4c121
Kevin Phillips <kphillips86@gmail.com>
2017-09-27 09:39:32 -0500

    Fixing support for Map and Set in IE11
    IE11 supports Map and Set but not Map.prototype.keys, Array.from, new Set(iterator), or Set.prototype[@@iterator].

--- types/set-test.js
+++ types/set-test.js
@@ -3,6 +3,14 @@ var shape = require("../reflections/shape/shape");
 var type = require("../reflections/type/type");
 require("./set");
 
+function assertSetMatchesArray(set, array, msg) {
+	QUnit.equal(set.size, array.length, msg + " - size matches");
+
+	for (var i=0; i<array.length; i++) {
+		QUnit.ok(set.has(array[i]), msg + " - set contains " + array[i]);
+	}
+}
+
 if(typeof Set !== "undefined") {
     QUnit.module("can-reflect/types/set Set");
 
@@ -14,7 +22,10 @@ if(typeof Set !== "undefined") {
 
     QUnit.test("shape.each", function(){
         var arr = ["a","b"];
-        var set = new Set(arr);
+        var set = new Set();
+        arr.forEach(function(val) {
+          set.add(val);
+        });
 
         var count = 0;
         shape.each(set, function(value){
@@ -27,19 +38,19 @@ if(typeof Set !== "undefined") {
 
         shape.update(set, ["a","a","c"]);
 
-        QUnit.deepEqual( Array.from(set), [
-            "a","c"
-        ], ".update");
+        assertSetMatchesArray(set, [ "a", "c" ], ".update");
     });
 
     QUnit.test("shape.assign", function(){
-        var set = new Set(["a","b"]);
+        var arr = ["a","b"];
+        var set = new Set();
+        arr.forEach(function(val) {
+          set.add(val);
+        });
 
         shape.assign(set, ["a","a","c"]);
 
-        QUnit.deepEqual( Array.from(set), [
-            "a","b","c"
-        ], ".assign");
+        assertSetMatchesArray(set, [ "a", "b", "c" ], ".assign");
     });
 }
 
