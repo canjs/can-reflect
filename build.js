a4c9e07c0d5bb13189dc5458ed4d05fb0bfca9ac
Justin Meyer <justinbmeyer@gmail.com>
2017-10-11 15:52:17 -0500

    DON'T USE, tries to build tests

--- build.js
+++ build.js
@@ -2,7 +2,8 @@ var stealTools = require("steal-tools");
 
 stealTools.export({
 	steal: {
-		config: __dirname + "/package.json!npm"
+		config: __dirname + "/package.json!npm",
+		main: "can-reflect/can-reflect-test"
 	},
 	outputs: {
 		"+cjs": {},
@@ -10,9 +11,9 @@ stealTools.export({
 		"+global-js": {}
 	}
 }).catch(function(e){
-	
+
 	setTimeout(function(){
 		throw e;
 	},1);
-	
+
 });
