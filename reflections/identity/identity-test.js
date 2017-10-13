a9bdd42bf598f9536126dfd3236b5f2d93388021
Christopher Baker <christopher@hmudesign.com>
2017-10-13 14:55:39 -0700

    fix tests for old IE

--- reflections/identity/identity-test.js
+++ reflections/identity/identity-test.js
@@ -2,6 +2,8 @@ var QUnit = require("steal-qunit");
 var canSymbol = require("can-symbol");
 var reflexions = require("./identity");
 
+var supportsFunctionName = (function name() {}).name === "name";
+
 QUnit.module("can-reflect: getIdentity");
 
 QUnit.test("it works with strings", function(assert) {
@@ -34,29 +36,31 @@ QUnit.test("it works with functions", function(assert) {
 	assert.equal(reflexions.getName(f), "Christopher");
 });
 
-QUnit.test("returns function name by default", function(assert) {
-	assert.equal(
-		reflexions.getName(function foo() {}),
-		"foo",
-		"should return function name"
-	);
-});
+if (supportsFunctionName) {
+	QUnit.test("returns function name by default", function(assert) {
+		assert.equal(
+			reflexions.getName(function foo() {}),
+			"foo",
+			"should return function name"
+		);
+	});
 
-QUnit.test("returns empty string for anonymous functions", function(assert) {
-	assert.equal(
-		reflexions.getName(function() {}),
-		"",
-		"should return empty string"
-	);
-});
+	QUnit.test("returns empty string for anonymous functions", function(assert) {
+		assert.equal(
+			reflexions.getName(function() {}),
+			"",
+			"should return empty string"
+		);
+	});
 
-QUnit.test("returns constructor name by default", function(assert) {
-	assert.equal(
-		reflexions.getName({}),
-		"Object{}",
-		"should return constructor name"
-	);
-});
+	QUnit.test("returns constructor name by default", function(assert) {
+		assert.equal(
+			reflexions.getName({}),
+			"Object{}",
+			"should return constructor name"
+		);
+	});
+}
 
 QUnit.test("handles list-likes (with identity)", function(assert) {
 	function ListThing(id) {
@@ -64,15 +68,27 @@ QUnit.test("handles list-likes (with identity)", function(assert) {
 	}
 	ListThing.prototype[canSymbol.for("can.isMoreListLikeThanMapLike")] = true;
 
-	assert.equal(
-		reflexions.getName(new ListThing()),
-		"ListThing[]",
-		"should use can.getName symbol behavior"
-	);
+	if (supportsFunctionName) {
+		assert.equal(
+			reflexions.getName(new ListThing()),
+			"ListThing[]",
+			"should use can.getName symbol behavior"
+		);
+	}
 
 	reflexions.setIdentity(ListThing.prototype, function() {
 		return this.id;
 	});
+
+	if (supportsFunctionName) {
+		assert.equal(
+			reflexions.getName(new ListThing(3)),
+			"ListThing[3]",
+			"should use can.getName symbol behavior with can.getIdentity"
+		);
+	}
+
+	reflexions.setName(ListThing, "ListThing");
 
 	assert.equal(
 		reflexions.getName(new ListThing(3)),
@@ -87,15 +103,27 @@ QUnit.test("handles map-likes (with identity)", function(assert) {
 	}
 	MapThing.prototype[canSymbol.for("can.isMapLike")] = true;
 
-	assert.equal(
-		reflexions.getName(new MapThing()),
-		"MapThing{}",
-		"should use can.getName symbol behavior"
-	);
+	if (supportsFunctionName) {
+		assert.equal(
+			reflexions.getName(new MapThing()),
+			"MapThing{}",
+			"should use can.getName symbol behavior"
+		);
+	}
 
 	reflexions.setIdentity(MapThing.prototype, function() {
 		return this.id;
 	});
+
+	if (supportsFunctionName) {
+		assert.equal(
+			reflexions.getName(new MapThing(3)),
+			"MapThing{3}",
+			"should use can.getName symbol behavior with can.getIdentity"
+		);
+	}
+
+	reflexions.setName(MapThing, "MapThing");
 
 	assert.equal(
 		reflexions.getName(new MapThing(3)),
@@ -110,15 +138,27 @@ QUnit.test("handles value-likes (with identity)", function(assert) {
 	}
 	ValueThing.prototype[canSymbol.for("can.isValueLike")] = true;
 
-	assert.equal(
-		reflexions.getName(new ValueThing()),
-		"ValueThing<>",
-		"should use can.getName symbol behavior"
-	);
+	if (supportsFunctionName) {
+		assert.equal(
+			reflexions.getName(new ValueThing()),
+			"ValueThing<>",
+			"should use can.getName symbol behavior"
+		);
+	}
 
 	reflexions.setIdentity(ValueThing.prototype, function() {
 		return this.id;
 	});
+
+	if (supportsFunctionName) {
+		assert.equal(
+			reflexions.getName(new ValueThing(3)),
+			"ValueThing<3>",
+			"should use can.getName symbol behavior with can.getIdentity"
+		);
+	}
+
+	reflexions.setName(ValueThing, "ValueThing");
 
 	assert.equal(
 		reflexions.getName(new ValueThing(3)),
