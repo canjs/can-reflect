1f826e059b3b941fc50cc8cfa5d2f5981688ad22
Justin Meyer <justinbmeyer@gmail.com>
2017-10-13 15:53:48 -0500

    1.6.0

---
+++ dist/cjs/can-reflect.js
@@ -0,0 +1,22 @@
+/*can-reflect@1.5.0#can-reflect*/
+var functionReflections = require('./reflections/call/call.js');
+var getSet = require('./reflections/get-set/get-set.js');
+var observe = require('./reflections/observe/observe.js');
+var shape = require('./reflections/shape/shape.js');
+var type = require('./reflections/type/type.js');
+var namespace = require('can-namespace');
+var reflect = {};
+[
+    functionReflections,
+    getSet,
+    observe,
+    shape,
+    type
+].forEach(function (reflections) {
+    for (var prop in reflections) {
+        reflect[prop] = reflections[prop];
+    }
+});
+require('./types/map.js');
+require('./types/set.js');
+module.exports = namespace.Reflect = reflect;
