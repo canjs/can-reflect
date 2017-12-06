2d4c53b317ff099876af333aaca17f655dd67817
Manuel Mujica <manuel.expat@gmail.com>
2017-12-05 18:26:44 -0700

    1.11.0

---
+++ dist/amd/reflections/helpers.js
@@ -0,0 +1,30 @@
+/*can-reflect@1.10.2#reflections/helpers*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-symbol'
+], function (require, exports, module) {
+    var canSymbol = require('can-symbol');
+    module.exports = {
+        makeGetFirstSymbolValue: function (symbolNames) {
+            var symbols = symbolNames.map(function (name) {
+                return canSymbol.for(name);
+            });
+            var length = symbols.length;
+            return function getFirstSymbol(obj) {
+                var index = -1;
+                while (++index < length) {
+                    if (obj[symbols[index]] !== undefined) {
+                        return obj[symbols[index]];
+                    }
+                }
+            };
+        },
+        hasLength: function (list) {
+            var type = typeof list;
+            var length = list && type !== 'boolean' && typeof list !== 'number' && 'length' in list && list.length;
+            return typeof list !== 'function' && (length === 0 || typeof length === 'number' && length > 0 && length - 1 in list);
+        }
+    };
+});
