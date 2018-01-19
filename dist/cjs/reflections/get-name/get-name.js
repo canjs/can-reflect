9181eed9f53e5e79c6b963993f3d85405105af4b
Matthew Phillips <matthew@matthewphillips.info>
2018-01-19 09:17:29 -0500

    1.13.1

---
+++ dist/cjs/reflections/get-name/get-name.js
@@ -0,0 +1,41 @@
+/*can-reflect@1.13.0#reflections/get-name/get-name*/
+var canSymbol = require('can-symbol');
+var typeReflections = require('../type/type.js');
+var getNameSymbol = canSymbol.for('can.getName');
+function setName(obj, nameGetter) {
+    if (typeof nameGetter !== 'function') {
+        var value = nameGetter;
+        nameGetter = function () {
+            return value;
+        };
+    }
+    Object.defineProperty(obj, getNameSymbol, { value: nameGetter });
+}
+function getName(obj) {
+    var nameGetter = obj[getNameSymbol];
+    if (nameGetter) {
+        return nameGetter.call(obj);
+    }
+    if (typeof obj === 'function') {
+        return obj.name;
+    }
+    if (obj.constructor && obj !== obj.constructor) {
+        var parent = getName(obj.constructor);
+        if (parent) {
+            if (typeReflections.isValueLike(obj)) {
+                return parent + '<>';
+            }
+            if (typeReflections.isMoreListLikeThanMapLike(obj)) {
+                return parent + '[]';
+            }
+            if (typeReflections.isMapLike(obj)) {
+                return parent + '{}';
+            }
+        }
+    }
+    return undefined;
+}
+module.exports = {
+    setName: setName,
+    getName: getName
+};
---
+++ dist/cjs/reflections/observe/observe.js
@@ -0,0 +1,88 @@
+/*can-reflect@1.13.0#reflections/observe/observe*/
+var canSymbol = require('can-symbol');
+var slice = [].slice;
+function makeFallback(symbolName, fallbackName) {
+    return function (obj, event, handler, queueName) {
+        var method = obj[canSymbol.for(symbolName)];
+        if (method !== undefined) {
+            return method.call(obj, event, handler, queueName);
+        }
+        return this[fallbackName].apply(this, arguments);
+    };
+}
+function makeErrorIfMissing(symbolName, errorMessage) {
+    return function (obj) {
+        var method = obj[canSymbol.for(symbolName)];
+        if (method !== undefined) {
+            var args = slice.call(arguments, 1);
+            return method.apply(obj, args);
+        }
+        throw new Error(errorMessage);
+    };
+}
+module.exports = {
+    onKeyValue: makeFallback('can.onKeyValue', 'onEvent'),
+    offKeyValue: makeFallback('can.offKeyValue', 'offEvent'),
+    onKeys: makeErrorIfMissing('can.onKeys', 'can-reflect: can not observe an onKeys event'),
+    onKeysAdded: makeErrorIfMissing('can.onKeysAdded', 'can-reflect: can not observe an onKeysAdded event'),
+    onKeysRemoved: makeErrorIfMissing('can.onKeysRemoved', 'can-reflect: can not unobserve an onKeysRemoved event'),
+    getKeyDependencies: makeErrorIfMissing('can.getKeyDependencies', 'can-reflect: can not determine dependencies'),
+    getWhatIChange: makeErrorIfMissing('can.getWhatIChange', 'can-reflect: can not determine dependencies'),
+    getChangesDependencyRecord: function getChangesDependencyRecord(handler) {
+        var fn = handler[canSymbol.for('can.getChangesDependencyRecord')];
+        if (typeof fn === 'function') {
+            return fn();
+        }
+    },
+    keyHasDependencies: makeErrorIfMissing('can.keyHasDependencies', 'can-reflect: can not determine if this has key dependencies'),
+    onValue: makeErrorIfMissing('can.onValue', 'can-reflect: can not observe value change'),
+    offValue: makeErrorIfMissing('can.offValue', 'can-reflect: can not unobserve value change'),
+    getValueDependencies: makeErrorIfMissing('can.getValueDependencies', 'can-reflect: can not determine dependencies'),
+    valueHasDependencies: makeErrorIfMissing('can.valueHasDependencies', 'can-reflect: can not determine if value has dependencies'),
+    onPatches: makeErrorIfMissing('can.onPatches', 'can-reflect: can not observe patches on object'),
+    offPatches: makeErrorIfMissing('can.offPatches', 'can-reflect: can not unobserve patches on object'),
+    onInstancePatches: makeErrorIfMissing('can.onInstancePatches', 'can-reflect: can not observe onInstancePatches on Type'),
+    offInstancePatches: makeErrorIfMissing('can.offInstancePatches', 'can-reflect: can not unobserve onInstancePatches on Type'),
+    onInstanceBoundChange: makeErrorIfMissing('can.onInstanceBoundChange', 'can-reflect: can not observe bound state change in instances.'),
+    offInstanceBoundChange: makeErrorIfMissing('can.offInstanceBoundChange', 'can-reflect: can not unobserve bound state change'),
+    isBound: makeErrorIfMissing('can.isBound', 'can-reflect: cannot determine if object is bound'),
+    onEvent: function (obj, eventName, callback, queue) {
+        if (obj) {
+            var onEvent = obj[canSymbol.for('can.onEvent')];
+            if (onEvent !== undefined) {
+                return onEvent.call(obj, eventName, callback, queue);
+            } else if (obj.addEventListener) {
+                obj.addEventListener(eventName, callback, queue);
+            }
+        }
+    },
+    offEvent: function (obj, eventName, callback, queue) {
+        if (obj) {
+            var offEvent = obj[canSymbol.for('can.offEvent')];
+            if (offEvent !== undefined) {
+                return offEvent.call(obj, eventName, callback, queue);
+            } else if (obj.removeEventListener) {
+                obj.removeEventListener(eventName, callback, queue);
+            }
+        }
+    },
+    setPriority: function (obj, priority) {
+        if (obj) {
+            var setPriority = obj[canSymbol.for('can.setPriority')];
+            if (setPriority !== undefined) {
+                setPriority.call(obj, priority);
+                return true;
+            }
+        }
+        return false;
+    },
+    getPriority: function (obj) {
+        if (obj) {
+            var getPriority = obj[canSymbol.for('can.getPriority')];
+            if (getPriority !== undefined) {
+                return getPriority.call(obj);
+            }
+        }
+        return undefined;
+    }
+};
