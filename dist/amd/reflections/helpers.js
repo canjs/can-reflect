c720cd5f7bb610559269daceb1ccdfc079f5ca65
Matthew Phillips <matthew@matthewphillips.info>
2018-06-18 12:55:57 -0400

    1.16.6

---
+++ dist/amd/reflections/helpers.js
@@ -0,0 +1,33 @@
+/*can-reflect@1.16.5#reflections/helpers*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-symbol'
+], function (require, exports, module) {
+    var canSymbol = require('can-symbol');
+    module.exports = {
+        makeGetFirstSymbolValue: function (symbolNames) {
+            var symbols = symbolNames.map(function (name) {
+                return canSymbol.for(name);
+            });
+            var length = symbols.length;
+            return function getFirstSymbol(obj) {
+                var index = -1;
+                while (++index < length) {
+                    if (obj[symbols[index]] !== undefined) {
+                        return obj[symbols[index]];
+                    }
+                }
+            };
+        },
+        hasLength: function (list) {
+            var type = typeof list;
+            if (type === 'string' || Array.isArray(list)) {
+                return;
+            }
+            var length = list && (type !== 'boolean' && type !== 'number' && 'length' in list) && list.length;
+            return typeof list !== 'function' && (length === 0 || typeof length === 'number' && length > 0 && length - 1 in list);
+        }
+    };
+});
