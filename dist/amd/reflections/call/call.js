5cc27efecc5bbbe92a67dba28c6d00f5fefc1a4e
mikedane <mikedane94@gmail.com>
2018-07-20 08:26:00 -0400

    1.17.1

---
+++ dist/amd/reflections/call/call.js
@@ -0,0 +1,46 @@
+/*can-reflect@1.17.0#reflections/call/call*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-symbol',
+    '../type/type'
+], function (require, exports, module) {
+    'use strict';
+    var canSymbol = require('can-symbol');
+    var typeReflections = require('../type/type');
+    module.exports = {
+        call: function (func, context) {
+            var args = [].slice.call(arguments, 2);
+            var apply = func[canSymbol.for('can.apply')];
+            if (apply) {
+                return apply.call(func, context, args);
+            } else {
+                return func.apply(context, args);
+            }
+        },
+        apply: function (func, context, args) {
+            var apply = func[canSymbol.for('can.apply')];
+            if (apply) {
+                return apply.call(func, context, args);
+            } else {
+                return func.apply(context, args);
+            }
+        },
+        'new': function (func) {
+            var args = [].slice.call(arguments, 1);
+            var makeNew = func[canSymbol.for('can.new')];
+            if (makeNew) {
+                return makeNew.apply(func, args);
+            } else {
+                var context = Object.create(func.prototype);
+                var ret = func.apply(context, args);
+                if (typeReflections.isPrimitive(ret)) {
+                    return context;
+                } else {
+                    return ret;
+                }
+            }
+        }
+    };
+});
