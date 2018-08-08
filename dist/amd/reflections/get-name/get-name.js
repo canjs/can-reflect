6f06dee7e58ae4cc23d0fc75440bcd020411924f
Matthew Phillips <matthew@matthewphillips.info>
2018-08-08 16:23:15 -0400

    1.17.2

---
+++ dist/amd/reflections/get-name/get-name.js
@@ -0,0 +1,54 @@
+/*can-reflect@1.17.1#reflections/get-name/get-name*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-symbol',
+    '../type/type'
+], function (require, exports, module) {
+    'use strict';
+    var canSymbol = require('can-symbol');
+    var typeReflections = require('../type/type');
+    var getNameSymbol = canSymbol.for('can.getName');
+    function setName(obj, nameGetter) {
+        if (typeof nameGetter !== 'function') {
+            var value = nameGetter;
+            nameGetter = function () {
+                return value;
+            };
+        }
+        Object.defineProperty(obj, getNameSymbol, { value: nameGetter });
+    }
+    function getName(obj) {
+        var type = typeof obj;
+        if (obj === null || type !== 'object' && type !== 'function') {
+            return '' + obj;
+        }
+        var nameGetter = obj[getNameSymbol];
+        if (nameGetter) {
+            return nameGetter.call(obj);
+        }
+        if (type === 'function') {
+            return obj.name;
+        }
+        if (obj.constructor && obj !== obj.constructor) {
+            var parent = getName(obj.constructor);
+            if (parent) {
+                if (typeReflections.isValueLike(obj)) {
+                    return parent + '<>';
+                }
+                if (typeReflections.isMoreListLikeThanMapLike(obj)) {
+                    return parent + '[]';
+                }
+                if (typeReflections.isMapLike(obj)) {
+                    return parent + '{}';
+                }
+            }
+        }
+        return undefined;
+    }
+    module.exports = {
+        setName: setName,
+        getName: getName
+    };
+});
