64428373cbab3e009d73771e04d00462d2b57cd6
Justin Meyer <justinbmeyer@gmail.com>
2018-08-27 15:13:44 -0500

    1.17.5

---
+++ dist/amd/types/set.js
@@ -0,0 +1,66 @@
+/*can-reflect@1.17.4#types/set*/
+define([
+    'require',
+    'exports',
+    'module',
+    '../reflections/shape/shape',
+    'can-symbol'
+], function (require, exports, module) {
+    'use strict';
+    var shape = require('../reflections/shape/shape');
+    var CanSymbol = require('can-symbol');
+    if (typeof Set !== 'undefined') {
+        shape.assignSymbols(Set.prototype, {
+            'can.isMoreListLikeThanMapLike': true,
+            'can.updateValues': function (index, removing, adding) {
+                if (removing !== adding) {
+                    shape.each(removing, function (value) {
+                        this.delete(value);
+                    }, this);
+                }
+                shape.each(adding, function (value) {
+                    this.add(value);
+                }, this);
+            },
+            'can.size': function () {
+                return this.size;
+            }
+        });
+        if (typeof Set.prototype[CanSymbol.iterator] !== 'function') {
+            Set.prototype[CanSymbol.iterator] = function () {
+                var arr = [];
+                var currentIndex = 0;
+                this.forEach(function (val) {
+                    arr.push(val);
+                });
+                return {
+                    next: function () {
+                        return {
+                            value: arr[currentIndex],
+                            done: currentIndex++ === arr.length
+                        };
+                    }
+                };
+            };
+        }
+    }
+    if (typeof WeakSet !== 'undefined') {
+        shape.assignSymbols(WeakSet.prototype, {
+            'can.isListLike': true,
+            'can.isMoreListLikeThanMapLike': true,
+            'can.updateValues': function (index, removing, adding) {
+                if (removing !== adding) {
+                    shape.each(removing, function (value) {
+                        this.delete(value);
+                    }, this);
+                }
+                shape.each(adding, function (value) {
+                    this.add(value);
+                }, this);
+            },
+            'can.size': function () {
+                throw new Error('can-reflect: WeakSets do not have enumerable keys.');
+            }
+        });
+    }
+});
