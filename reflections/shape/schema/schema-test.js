c03d172c2b81aeddc19c46acaee1990c3f9908c4
Bradley Momberger <brad@bitovi.com>
2019-05-21 23:07:48 -0400

    Add a dates test

--- reflections/shape/schema/schema-test.js
+++ reflections/shape/schema/schema-test.js
@@ -60,6 +60,20 @@ QUnit.test('cloneKeySort with strings', function (assert) {
     assert.equal( JSON.stringify( schemaReflections.cloneKeySort(obj) ), JSON.stringify( same ) );
 });
 
+QUnit.test('cloneKeySort with dates', function (assert) {
+    var obj = {
+        "z": new Date(1999000),
+        "a": new Date(2001000)
+    };
+
+    var same = {
+        "a": new Date(2001000),
+        "z": new Date(1999000)
+    };
+
+    assert.equal( JSON.stringify( schemaReflections.cloneKeySort(obj) ), JSON.stringify( same ) );
+});
+
 QUnit.test("getIdentity", function(){
 
     var value = new MyType(5);
