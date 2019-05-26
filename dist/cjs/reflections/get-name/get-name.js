55f4878ef49b33a5b3a798bf3ab482a565c6ac28
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-05-26 22:49:13 +0100

    1.17.11

---
+++ dist/cjs/reflections/get-name/get-name.js
@@ -0,0 +1,50 @@
+/*can-reflect@1.17.10#reflections/get-name/get-name*/
+'use strict';
+var canSymbol = require('can-symbol');
+var typeReflections = require('../type/type.js');
+var getNameSymbol = canSymbol.for('can.getName');
+function setName(obj, nameGetter) {
+    if (typeof nameGetter !== 'function') {
+        var value = nameGetter;
+        nameGetter = function () {
+            return value;
+        };
+    }
+    Object.defineProperty(obj, getNameSymbol, { value: nameGetter });
+}
+var anonymousID = 0;
+function getName(obj) {
+    var type = typeof obj;
+    if (obj === null || type !== 'object' && type !== 'function') {
+        return '' + obj;
+    }
+    var nameGetter = obj[getNameSymbol];
+    if (nameGetter) {
+        return nameGetter.call(obj);
+    }
+    if (type === 'function') {
+        if (!('name' in obj)) {
+            obj.name = 'functionIE' + anonymousID++;
+        }
+        return obj.name;
+    }
+    if (obj.constructor && obj !== obj.constructor) {
+        var parent = getName(obj.constructor);
+        if (parent) {
+            if (typeReflections.isValueLike(obj)) {
+                return parent + '<>';
+            }
+            if (typeReflections.isMoreListLikeThanMapLike(obj)) {
+                return parent + '[]';
+            }
+            if (typeReflections.isMapLike(obj)) {
+                return parent + '{}';
+            }
+        }
+    }
+    return undefined;
+}
+module.exports = {
+    setName: setName,
+    getName: getName
+};
